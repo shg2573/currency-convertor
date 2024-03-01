e6ba99c2c10d3e60d19ee8cc0120da6f851fc445
Shreyash <shreyash2573@gmail.com>
2024-03-02 03:03:42 +0530

    input box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Input from './components/Input';
 
 function App() {
+  const dataSet=[{labels:"From"},{labels:"To"}]
   return (
-    <div className="App">
-      <div className='text-red-600 bg-gradient-to-tr from-black  via-slate-900  to-black  h-screen w-screen'>
-        <div className='h-24 w-40'><Input {..."From"}/></div>
+    <div className="App ">
+      <div className='text-red-600 flex-col  bg-gradient-to-tr from-black  via-slate-900  to-black  h-screen w-screen flex justify-center items-center'>
+        <div className=' h-fit w-80 '><Input {...dataSet[0]}/></div>
+        <button /*onClick={}*/ className='px-2 py-1 bg-blue-500 border-2 border-transparent hover:border-white text-white rounded-xl'>Swap</button>
+        <div className=' h-fit w-80 '><Input {...dataSet[1]}/></div>
       </div>
     </div>
   );
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const Input = (props) => {
+const Input = ({
+  labels,
+  amount,
+  setAmountOn,
+
+  className=""
+}) => {
+
   return (
           <div className={`bg-white p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
                 <label  className="text-black/40 mb-2 inline-block">
-                    label
+                    {labels}
                 </label>
                 <input
                     
